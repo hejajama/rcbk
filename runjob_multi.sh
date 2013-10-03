5e38bdde8687ce8efd56f7d22242262f96c112bc
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2013-10-03 15:40:16 +0300

    Minor changes to running scripts

--- runjob_multi.sh
+++ runjob_multi.sh
@@ -1,18 +1,18 @@
 LC_ALL="C"
-for qsqr in 0.1580 0.1590 0.1600 0.1610  #0.1605 0.1610 0.1615 0.162 0.1625 0.1630 0.1633 0.1634 0.1635 0.1636 0.1637 ## 0.167 0.168 0.169
+ic="mv1"
+for qsqr in `seq -f %0.4f 0.0500 0.0100 0.3` # 0.3000` 
 do
-	for csqr in 6.7000 6.8000 6.9000 7.0000 7.1000 #5.8 5.9 6.0 6.1 6.2 6.3 6.4 6.5  #5 5.25 5.5 5.75 6  #5.5 6 6.5 7 #0.3152 
+	for csqr in `seq -f %07.4f 2.0000 2 40.0000`
 	do
-		for gamma in 1.1100 1.1150 1.1200 1.1250 1.1300 1.1350 1.1400 1.1450 #1.1 1.125 1.15 1.175 #1.135 # 1.1 1.2 1.3 1.4 1.5  #1.05 1.1 1.15 # 1.1 1.2
+		for gamma in "01.0000"
 		do
-			for ec in 1 #5.65 5.7 5.75 #5 10 1.175#8 9 10 11 12 13
+			for ec in "01.0000" #`seq -f %07.4f 1.0000 4 30` # 50.0000` #5.65 5.7 5.75 #5 10 1.175#8 9 10 11 12 13
 			do
 				for freeze in 0 
 				do
-					data="fit/aamqs/mv_qsqr_${qsqr}_gamma_${gamma}_csqr_${csqr}_ec_${ec}_freeze_${freeze}.dat"
+					data="/work/hejajama/herafit_systemaattisesti/${ic}/mv_qsqr_${qsqr}_gamma_${gamma}_csqr_${csqr}_ec_${ec}_freeze_${freeze}.dat"
 					if [ ! -f $data ]
 					then
-						touch ${data}
 						out="fit/stdouterr/mv_qsqr_${qsqr}_gamma_${gamma}_csqr_${csqr}_ec_${ec}_freeze_${freeze}.stdout"
 						err="fit/stdouterr/mv_qsqr_${qsqr}_gamma_${gamma}_csqr_${csqr}_ec_${ec}_freeze_${freeze}.stderr"
 						cmd="-J rbk_qsqr_${qsqr}_gamma_${gamma}_csqr_${csqr}_ec_${ec}_freeze_${freeze} -o ${out} -e ${err} ./runjob_param.sh ${qsqr} ${gamma} 0.01 ${csqr} ${ec} ${freeze} ${data}"
--- runjob_param.sh
+++ runjob_param.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SBATCH -J rbk
-#SBATCH -t 30:00:00
+#SBATCH -t 10:00:00
 #SBATCH -p normal
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=6
+#SBATCH --ntasks-per-node=4
 LC_ALL="C"
-OMP_NUM_THREADS=6
-#setenv OMP_NUM_THREADS 12
-export OMP_NUM_THREADS=6
+OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=4
+module add intel
 ##parametrit: Qs^2 gamma x0 C^2 ec freeze output
+echo Running on `hostname`
+echo Modules:
+echo `module list`
 #if [ ! -f ${7} ]
 #then
-	./rbk -ic MV $1 $2 $3 $5 -alphas_scaling $4 -minr 1e-6 -rc BALITSKY -alphas_freeze_c ${6} -output ${7} -maxy 16 -ystep 0.2
+	./rbk -ic MV $1 $2 $3 $5 -alphas_scaling $4 -minr 1e-6 -rc BALITSKY -alphas_freeze_c ${6} -output ${7} -maxy 11 -ystep 0.2
 #fi
 
