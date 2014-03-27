6632a45dc75739a4857cdfa433c87b6583421229
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2014-03-27 12:58:39 +0200

    Add missing file

---
+++ src/config.hpp
@@ -0,0 +1,15 @@
+/*
+ * BK equation solver
+ * Heikki Mäntysaari <heikki.mantysaari@jyu.fi>, 2011-2013
+ */
+
+#ifndef _BKCONFIG_HPP
+#define _BKCONFIG_HPP
+
+typedef double REAL;
+const int Nc=3;
+const int Nf=3;
+const double LAMBDAQCD=0.241;
+
+#endif
+
